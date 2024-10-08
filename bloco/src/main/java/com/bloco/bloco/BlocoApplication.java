2818e2edd346679adf1fd1d0fe3a3fa3e42580cd
randsantana <randerson_santana@estudante.sesisenai.org.br>
2024-10-07 21:46:21 -0300

    feat: add model pedidos

---
+++ bloco/src/main/java/com/bloco/bloco/BlocoApplication.java
@@ -0,0 +1,13 @@
+package com.bloco.bloco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlocoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlocoApplication.class, args);
+	}
+
+}
---
+++ bloco/src/test/java/com/bloco/bloco/BlocoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bloco.bloco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlocoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
